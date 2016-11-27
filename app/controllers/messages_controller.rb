4057940d8aa77ebabef7ba5d5b1b65e584ba0de6
Daiki Nagaoka <Daiki@lneck.local>
2016-11-27 11:27:29 +0900

    メッセージ投稿〜メッセージ一覧

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,36 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
 
   def index
+    @groups = current_user.groups
+    @user_names = @group.users.map(&:nickname)
     @message = Message.new
-    @messages = Message.all
-    @groups = Group.all
+    @messages = @group.messages.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @messages.includes(:user).map(&:to_api_json) }
+    end
   end
 
   def create
-    message = Message.new(message_params)
-    if message.save
-      redirect_to root_path, notice: 'SUCCESS'
-    else
-      redirect_to root_path, alert: 'エラー'
+    @message = @group.messages.create(message_params)
+    respond_to do |format|
+      format.html do
+        redirect_to group_messages_path(group), notice: 'メッセージを送信しました'
+      end
+      format.json do
+        render json: @message.to_api_json
+      end
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:text).merge(user_id: current_user.id, group_id: 1)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 
 end
